ccf86b4bd75ea454c290db9289dbdace8f535db7
becca107 <odouarebecca@gmail.com>
2025-06-13 19:08:23 +0100

    Add initial project structure and main application files

--- app/src/main/java/com/example/myapplication/controller/GameActivity.java
+++ app/src/main/java/com/example/myapplication/controller/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.controller;
+
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.content.Intent; // <-- Ajouté
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -17,6 +18,8 @@ import com.example.myapplication.model.Question;
 import com.example.myapplication.model.QuestionBank;
 
 import java.util.Arrays;
+
+
 public class GameActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView m_textViewQuestion;
@@ -33,7 +36,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private boolean mEnableTouchEvents;
     public static final String BUNDLE_STATE_SCORE = "BUNDLE_STATE_SCORE";
     public static final String BUNDLE_STATE_QUESTION = "BUNDLE_STATE_QUESTION";
-    private static final int QUESTION_COUNT = 6;
+
 
     private QuestionBank generateQuestions() {
         Question question1 = new Question(
@@ -181,6 +184,38 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 question9, question10, question11, question12, question13));
     }
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mRemainingQuestionCount = 6; // Set the number of questions to be asked
+        mCurrentQuestion = mQuestionBank.getCurrentQuestion();
+        mScore = 0; // Initialize the score
+
+        if(savedInstanceState != null){
+            mScore = savedInstanceState.getInt(BUNDLE_STATE_SCORE);
+            mRemainingQuestionCount = savedInstanceState.getInt(BUNDLE_STATE_QUESTION);
+        }else{
+            mScore = 0;
+            mRemainingQuestionCount = 4;
+        }
+
+
+        m_textViewQuestion = findViewById(R.id.game_activity_textview_question);
+        mButton1 = findViewById(R.id.game_activity_button_1);
+        mButton2 = findViewById(R.id.game_activity_button_2);
+        mButton3 = findViewById(R.id.game_activity_button_3);
+        mButton4 = findViewById(R.id.game_activity_button_4);
+
+        mButton1.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+        mButton3.setOnClickListener(this);
+        mButton4.setOnClickListener(this);
+
+        displayQuestion(mCurrentQuestion);
+    }
+    @Override
     public void onClick(View v) {
         int index;
         if (v == mButton1) {
@@ -194,15 +229,13 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             throw new IllegalStateException("Unknown clicked view: " + v);
         }
-
-        // Vérifie la réponse
+        // Check if the answer is correct
         if (index == mQuestionBank.getCurrentQuestion().getAnswerIndex()) {
             Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
-            mScore++;
+            mScore++; // Increment the score for a correct answer
         } else {
             Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
         }
-
         mEnableTouchEvents = false;
 
         new Handler(getMainLooper()).postDelayed(new Runnable() {
@@ -210,28 +243,40 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 mEnableTouchEvents = true;
                 mRemainingQuestionCount--;
-                if (mRemainingQuestionCount > 0) {
+                if (mRemainingQuestionCount <= 0) {
                     mCurrentQuestion = mQuestionBank.getNextQuestion();
                     displayQuestion(mCurrentQuestion);
                 } else {
-                    // Fin du quiz
-                    AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
-                    builder.setTitle("Well done!");
-                    builder.setMessage("Your score is: " + mScore + "/6");
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent();
-                            intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
-                            setResult(RESULT_OK, intent);
-                            finish();
-                        }
-                    });
-                    builder.setCancelable(false);
-                    builder.show();
+                    Intent intent = new Intent();
+                    intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                    setResult(RESULT_OK, intent);
+                    finish();
+
                 }
             }
         }, 2000);
+
+        mRemainingQuestionCount--;
+        if (mRemainingQuestionCount > 0) {
+            mCurrentQuestion = mQuestionBank.getNextQuestion();
+            displayQuestion(mCurrentQuestion);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Well done!");
+            builder.setMessage("Your score is: " + mScore + "/" + 6);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent();
+                    intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            });
+            builder.create();
+            builder.show();
+        }
+
     }
     private void displayQuestion(final Question question) {
         m_textViewQuestion.setText(question.getQuestion());
